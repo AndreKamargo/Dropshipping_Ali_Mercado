b74da92e919b025e29653a920033468878fe5522
AndreKamargo <99135120+AndreKamargo@users.noreply.github.com>
2022-02-07 16:16:45 -0300

    Update Dropshipping_Ali_MercadoLivre.py

--- Dropshipping_Ali_MercadoLivre.py
+++ Dropshipping_Ali_MercadoLivre.py
@@ -1,5 +1,12 @@
+def real(valor):
+  a = "{:,.2f}".format(float(valor))
+  b = a.replace(',','v')
+  c = b.replace('.',',')
+  return c.replace('v','.')
+
 Nome = input("Qual o nome do produto?: ")
 Valor = input("Qual o valor do produto: ")
+Valor = Valor.replace(".","")
 Valor = Valor.replace(",",".")
 Valor = float(Valor)
 
@@ -9,23 +16,24 @@ ICMS = (Valor*18)/100
 Premium = (Valor*16)/100
 Frete = Valor + 30
 Total = Valor + 15 + Taxado + ICMS + Premium + 30
+TotalImposto = Valor + 15 + Premium + 30
 
-
-print("\nNome do produto :",Nome)
-print("Valor do produto no Alixpress :",Valor)
 print("\nTaxas:")
-print("Despacho -R$15 = ",Despacho)
-print("Caso taxado, taxa maxima de 60% = ",Taxado)
-print("ICMS de SP 18% = ",ICMS)
-print("Mercado Premium 16% = ",Premium)
-print("Frete gratis -R$30 = ",Frete)
+print("Despacho -R$15 = ",real(Despacho))
+print("Caso taxado, taxa maxima de 60% = ",real(Taxado))
+print("ICMS de SP 18% = ",real(ICMS))
+print("Mercado Premium 16% = ",real(Premium))
+print("Frete gratis -R$30 = ",real(Frete))
 if Valor < 120:
     Menor = Valor + 5
     Total = Total + 5
-    print("Valor abaixo de 120 -R$5 = ",Menor)
-    print("\nValor final = ", Total)
+    TotalImposto = TotalImposto + 5
+    print("Valor abaixo de 120 -R$5 = ",real(Menor))
+    print("\nValor final = ", real(Total))
+    print("\nValor final sem impostos = ",real(TotalImposto))
 else:
-    print("\nValor final = ",Total)
+    print("\nValor final = ",real(Total))
+    print("Valor final sem impostos = ", real(TotalImposto))
 
 
 
